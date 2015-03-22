e4549d2b0a0b847b4587b11a07e11ce67781de16
Axel Rivera <axel@axelrivera.me>
2015-03-21 22:29:17 -0400

    Remove crap

--- .bash_profile
+++ .bash_profile
@@ -3,3 +3,6 @@ for file in ~/.{extras,bash_prompt,exports,aliases,functions,secret}; do
     [ -r "$file" ] && source "$file"
 done
 unset file
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+[[ -r $rvm_path/scripts/completion ]] && . $rvm_path/scripts/completion
--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew doctor
 
 # Install cask
-# brew install caskroom/cask/brew-cask
+brew install caskroom/cask/brew-cask
 
 # Alternative cask versions
 brew tap caskroom/versions
@@ -24,64 +24,24 @@ brew cask install font-sauce-code-powerline
 
 # Install apps with Cask
 #echo "Installing OS X Apps..."
-brew cask install slack
-brew cask install dropbox
-# brew cask install evernote
-# brew cask install harvest
-# brew cask install spotify
-# brew cask install transmission
-# brew cask install paparazzi
-# brew cask install viscosity
-# brew cask install mailbox
 brew cask install gpgtools
-
-# brew cask install sublime-text3
+brew cask install sublime-text3
 brew cask install iterm2
 brew cask install heroku-toolbelt
 brew cask install virtualbox
 brew cask install vagrant
-brew cask install rowanj-gitx
-# brew cask install robomongo
 brew cask install macdown
-# brew cask install cyberduck
 brew cask install postgres
-# brew cask install macvim
 
 # Install development tools with Homebrew
 echo "Installing development tools..."
 brew install bash-completion
 brew install git
-# brew install python3
-# brew install python
-# brew install go
 brew install the_silver_searcher
 
-# Install nvm
-# echo "Installing nvm..."
-# curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | PROFILE=~/.extras bash
-# source ~/.bash_profile
-
-# Install latest v0.10.x release of node
-# echo "Installing node..."
-# nvm install 0.10
-# nvm alias default 0.10
-
-# Install Node.js global tools
-# echo "Installing global modules with npm..."
-# npm install -g npm
-# npm install -g bower
-# npm install -g ember-cli
-# npm install -g grunt-cli
-# npm install -g gulp
-# npm install -g jshint
-
-# Install Python tools
-# echo "Installing Python tools"
-# mkdir ~/.virtualenvs
-# pip install virtualenvwrapper
-# pip3 install flake8
+source ~/.bash_profile
 
 # Setup Sublime Text 3
-# mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-# ln -sf $PWD/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-# ln -sf $PWD/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+mkdir -p ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
+ln -sf $PWD/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -sf $PWD/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
